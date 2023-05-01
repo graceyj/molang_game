6c6fcff9dc47d84a436907e57ecd280fb4af5f51
유진01 <121075832+graceyj@users.noreply.github.com>
2023-05-01 20:22:17 +0900

    jq update01

--- js/molang.js
+++ js/molang.js
@@ -1,8 +1,23 @@
 $(function(){
-    $(document).on('touchstart',function(){});
     $('html,body').animate({scrollTop:'3px'}, 300);
-    $('.start').on('click',function(){
+    
+    $('.start').click(function(){
         $('#sect01').hide();
         $('#sect02').show();
     });
+    
+    $('#sect02, #sect03').click(function(){
+        $(this).hide().next().show();
+    }); 
+    
+    $('#sect04').click(function() {
+        $('.say01').hide();
+        $('.say02').show();
+        $(this).off('click').click(function(){
+            $('#sect04').hide();
+            $('#sect05').show();
+        });
+    });
+
+    $(document).on('touchstart', function(){});
 });
